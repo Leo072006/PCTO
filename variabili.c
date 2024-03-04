4b8d61786c4f5a5a667fac9f12ea06caf8b749e2
Leo072006 <149933371+Leo072006@users.noreply.github.com>
2024-03-04 16:01:44 +0000

    github

---
+++ variabili.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+ 
+int main()
+{
+    /* printf("ciao\n");
+    int numero;
+    numero = 20;
+    printf("%d\n", numero);
+    char lettera;
+    lettera = 'L';
+    printf("%d\n", lettera);*/
+    int num_1 = 43;
+    int num_2 = 71;
+    int result = num_1 + num_2;
+    printf("%d\n", result);
+    int num_3 = 43;
+    int num_4 = 71;
+    int result1 = num_3 - num_4;
+    printf("%d\n", result1);
+    int num_5 = 43;
+    int num_6 = 71;
+    int result2 = num_5 * num_6;
+    printf("%d\n", result2);
+    int num_7 = 100;
+    int num_8 = 41;
+    float result3 = num_7 / num_8;
+    printf("%f\n", result3);
+    int num_9 = 100;
+    int num_10 = 41;
+    float result4 = num_9 % num_10;
+    printf("%f\n", result4);
+    return(0);
+} 
+
+
